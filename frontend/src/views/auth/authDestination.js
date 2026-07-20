5cca8b4cb4011219f90331c069c359099c036341
BigSlickGames <bigslickgames@gmail.com>
2026-07-20 22:35:02 +1000

    Route auth back to BSG lobby

--- frontend/src/views/auth/authDestination.js
+++ frontend/src/views/auth/authDestination.js
@@ -1,19 +1,10 @@
+const DEFAULT_BSG_HOME_URL = 'https://bigslickgames.com';
+
 export function getBigSlickGamesUrl() {
     const configuredUrl = process.env.REACT_APP_BSG_HOME_URL;
     if (configuredUrl) return configuredUrl.replace(/\/$/, '');
 
-    if (typeof window === 'undefined') return '/';
+    if (typeof window === 'undefined') return DEFAULT_BSG_HOME_URL;
 
-    const currentUrl = new URL(window.location.href);
-    const gameDevPorts = new Set(['3000', '3003', '3100']);
-
-    if (gameDevPorts.has(currentUrl.port)) {
-        currentUrl.port = '3200';
-        currentUrl.pathname = '/';
-        currentUrl.search = '';
-        currentUrl.hash = '';
-        return currentUrl.toString().replace(/\/$/, '');
-    }
-
-    return window.location.origin || '/';
+    return DEFAULT_BSG_HOME_URL;
 }
